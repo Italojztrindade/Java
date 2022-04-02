944cfbc55b0c431f23c4600a08e5cc0e56786118
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2022-04-01 21:30:25 -0300

    Update AreaCircunferencia.java

--- Exercicios/src/fundamentos/AreaCircunferencia.java
+++ Exercicios/src/fundamentos/AreaCircunferencia.java
@@ -7,9 +7,14 @@ public class AreaCircunferencia {
 	 */	
 	public static void main(String[] args) {
 		double raio = 3.4;
-		final double pi = 3.14159;		
-		double area = pi * raio * raio;
+		final double PI = 3.14159;		
+		double area = PI * raio * raio;
 		
-		System.out.println(area);
+		System.out.println("Área = " + area);
+		
+		raio = 10;
+		
+		area = PI * raio * raio;
+		System.out.println("Área = " + area);
 	}
 }
