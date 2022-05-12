fa0a7d42880d2fb9ff418bba75607451edd930de
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2022-05-12 20:01:50 -0300

    resolução exercício 01
    Resolução do exercício de coleção 01

--- DIO/src/collections/ExercicioProposto01.java
+++ DIO/src/collections/ExercicioProposto01.java
@@ -3,34 +3,73 @@ package collections;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 public class ExercicioProposto01 {
-	/*Faça um programa que receba a temperatura média os 6 primeiros meses do
-	ano e armazene-as em uma lista.Após isto, calcule a média semestral 
-	das temperaturas e mostre todas as temperaturas acima desta média,
-	e em que mês elas ocorreram (mostrar o mês por
-	extenso: 1 – Janeiro, 2 – Fevereiro e etc).
-	*/
-	
+	/*
+	 * Faça um programa que receba a temperatura média os 6 primeiros meses do ano e
+	 * armazene-as em uma lista.Após isto, calcule a média semestral das
+	 * temperaturas e mostre todas as temperaturas acima desta média, e em que mês
+	 * elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro e etc).
+	 */
+
 	public static void main(String[] args) {
-		
+
+		Scanner entrada = new Scanner(System.in);
+
 		List<Double> temperaturas = new ArrayList<Double>();
-		
-		temperaturas.add(0, 30.9);
-		temperaturas.add(0, 20.1);
-		temperaturas.add(0, 10.9);
-		temperaturas.add(0, 15.8);
-		temperaturas.add(0, 25.3);
-		temperaturas.add(0, 5d);
-		
+
+		System.out.println("Informe as temperaturas: ");
+		temperaturas.add(entrada.nextDouble());
+		temperaturas.add(entrada.nextDouble());
+		temperaturas.add(entrada.nextDouble());
+		temperaturas.add(entrada.nextDouble());
+		temperaturas.add(entrada.nextDouble());
+		temperaturas.add(entrada.nextDouble());
+
+		System.out.println(temperaturas);
 		Iterator<Double> iterator = temperaturas.iterator();
-		
+
 		double soma = 0;
-		
-		while(iterator.hasNext()) {
+		double media = 0;
+		while (iterator.hasNext()) {
 			Double next = iterator.next();
 			soma += next;
 		}
-	
+
+		media = soma / temperaturas.size();
+		System.out.printf("A média do semestre é de: %.1f ºC", media);
+
+		System.out.println("#######");
+		for (Double double1 : temperaturas) {
+			if (double1 >= media) {
+				int index = temperaturas.indexOf(double1);
+				switch (index) {
+				case 0:
+					System.out.println((index + 1) + " - JANEIRO: " + double1 + " ºC");
+					break;
+				case 1:
+					System.out.println((index + 1) + " - FEVEREIRO: " + double1 + " ºC");
+					break;
+				case 2:
+					System.out.println((index + 1) + " - MARÇO: " + double1 + " ºC");
+					break;
+				case 3:
+					System.out.println((index + 1) + " - ABRIL: " + double1 + " ºC");
+					break;
+				case 4:
+					System.out.println((index + 1) + " - MAIO: " + double1 + " ºC");
+					break;
+				case 5:
+					System.out.println((index + 1) + " - JUNHO: " + double1 + " ºC");
+					break;
+				default:
+					break;
+				}
+			}
+		}
+
+		entrada.close();
 	}
+
 }
