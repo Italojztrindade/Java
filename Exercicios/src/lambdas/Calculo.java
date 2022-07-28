ce7f07be6a5eff326eb9a696a2db125c2df589ad
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2022-07-28 19:26:55 -0300

    Funções lambda
    Funções lambda

--- Exercicios/src/lambdas/Calculo.java
+++ Exercicios/src/lambdas/Calculo.java
@@ -1,7 +1,20 @@
 package lambdas;
 
+@FunctionalInterface
+//Só permite usar um método/função abstrato na interface 
+//com exceção de metodo padrão(default) e static
+
+// dica das funcionalidades 
+//https://docs.oracle.com/javase/8/docs/api/java/util/function/package-summary.html
 public interface Calculo {
 
 	double executar(double a, double b);
+	
+//	default String legal() {
+//		return "legal";
+//	}
+//	static String muitoLegal() {
+//		return "muitoLegal";
+//	}
 	
 }
