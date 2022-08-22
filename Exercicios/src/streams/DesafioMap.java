145d55a217fc4d7c94c174f838ff7a78903be4c3
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2022-08-22 20:37:22 -0300

    Desafio Map
    Primeira parte: ok

---
+++ Exercicios/src/streams/DesafioMap.java
@@ -0,0 +1,28 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class DesafioMap {
+
+	public static void main(String[] args) {
+		
+		Consumer<String> print = System.out::println;
+		
+		List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		
+		/*
+		 * 1. Número para string binária... 6 => "110"
+		 * 2. Reverter a string... "110" => "011"
+		 * 3. Converter de volta para inteiro => "011" => 3
+		 */
+		
+		nums.stream().map(m -> m.toBinaryString(m)).forEach(print);
+		
+		
+		
+		
+	}
+	
+}
