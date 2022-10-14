bce379f8beafc61ea608e1fee48beb760d66e543
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2022-10-13 21:14:14 -0300

    Criação da classe Tabuleiro e Aplicação
    Criação das classes tabuleiro e aplicação.

---
+++ campo-minado+JUnit/src/br/com/cod3r/cm/Aplicacao.java
@@ -0,0 +1,15 @@
+package br.com.cod3r.cm;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		
+		tabuleiro.abrir(3, 3);
+		tabuleiro.altenarMarcacao(4, 4);
+		tabuleiro.altenarMarcacao(4, 5);
+		System.out.println(tabuleiro);
+	}
+}
