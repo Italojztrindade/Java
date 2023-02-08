1e9ef95d52c8274cc7959a11863cccc7bb4d1016
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2023-02-07 21:39:15 -0300

    Requires Transitive e Export To

--- app_calculo/src/module-info.java
+++ app_calculo/src/module-info.java
@@ -1,4 +1,8 @@
 module app.calculo {
-	
+	//transitive o que você recebeu de um modulo pode ser passado para outro de modo transitivo
+	requires transitive app.logging;
 	exports br.com.cod3r.app.calculo;
+	
+	exports br.com.cod3r.app.calculo.interno
+		to app.financeiro;
 }
