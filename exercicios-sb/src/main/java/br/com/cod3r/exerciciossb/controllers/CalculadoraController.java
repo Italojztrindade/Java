8dcec4448e4261e25792d140981ff82b6bdad6a0
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2023-05-02 07:27:17 -0300

    update CalculadoraController

--- exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/CalculadoraController.java
+++ exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/controllers/CalculadoraController.java
@@ -28,4 +28,18 @@ public class CalculadoraController {
 		
 	}
 	
+	
+//	resolução 
+//	@GetMapping("/somar/{a}/{b}")
+//	public int somar(@PathVariable int a, @PathVariable int b) {
+//		return a + b;
+//	}
+//	
+//	
+//	// /calculadora/subtrair?a=100&b=39
+//	@GetMapping("/subtrair")
+//	public int subtrair(@RequestParam(name="a") int a, @RequestParam(name="b") int b) {
+//		return a - b;
+//	}
+	
 }
